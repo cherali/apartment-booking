26a87230fcc7753986275e0f35edf8a5a4ef42da
Ali.Cheraghi <alicheraghi148@gmail.com>
2023-10-06 16:43:35 +0330

    move stateManagerProvider to main.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,10 @@
 import { Suspense } from 'react'
 import AppRouter from './Routes/AppRouter'
-import StateManagerProvider from './providers/StateManagerProvider/StateManagerProvider'
 
 function App() {
   return (
     <Suspense fallback={<p>loading...</p>}>
-      <StateManagerProvider>
-        <AppRouter />
-      </StateManagerProvider>
+      <AppRouter />
     </Suspense>
   )
 }
